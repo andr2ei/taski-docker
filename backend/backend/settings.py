a3917e8acb378bb8036a1314d936aabda82add2d
Andrei Andronov <ao.andronov@gmail.com>
2024-03-29 12:44:54 +0300

    fixing POST method

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,15 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = ('django-insecure-j_89af+30&&4qm*8z9_'
-              '(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1~frontend~nginx').split('~')
 
 
 # Application definition
